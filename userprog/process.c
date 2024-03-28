db52f79e7a6cfae084f45797871d347d1efe535a
haaaabin <chabin7181@gmail.com>
2024-03-29 00:58:17 +0900

    anoynmous page - load_segment, lazy_load_segment 구현

--- userprog/process.c
+++ userprog/process.c
@@ -661,6 +661,24 @@ lazy_load_segment (struct page *page, void *aux) {
 	/* 1. 파일에서 세그먼트를 로드합니다.
 	   2. 주소 VA에서 첫 번째 페이지 오류가 발생하면 호출됩니다. 
 	   3. 이 함수를 호출할 때 VA를 사용할 수 있다.*/
+
+	struct file * file = ((void **)aux)[0];
+	size_t page_read_bytes = ((size_t **)aux)[1];
+	size_t page_zero_bytes = ((size_t **)aux)[2];
+
+	/* Get a page of memory. */
+	uint8_t *kpage = palloc_get_page (PAL_USER);
+	if (kpage == NULL)
+		return false;
+
+	/* Load this page. */
+	if (file_read (file, kpage, page_read_bytes) != (int) page_read_bytes) {
+		palloc_free_page (kpage);
+		return false;
+	}
+	memset (kpage + page_read_bytes, 0, page_zero_bytes);
+
+	return true;
 }
 
 /* Loads a segment starting at offset OFS in FILE at address
@@ -709,6 +727,7 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
 		/* TODO: Set up aux to pass information to the lazy_load_segment. */
 		/* lazy_load_segment에 정보를 전달하도록 aux를 설정합니다.*/
 		void *aux = NULL;
+		aux = (file, page_read_bytes, page_zero_bytes);
 		if (!vm_alloc_page_with_initializer (VM_ANON, upage,
 					writable, lazy_load_segment, aux))
 			return false;
